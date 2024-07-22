2e239320038dc40916b138e4a4b285b7fa4d92c2
poojakhare1 <147541085+poojakhare1@users.noreply.github.com>
2024-07-22 09:45:27 +0530

    Add files via upload

---
+++ loop2.java
@@ -0,0 +1,10 @@
+public class loop2 {
+    public ststic void main(String args[]){
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i = i+1;
+        }
+    }
+    
+}
