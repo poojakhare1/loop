1b05cc7ea004437799e948d1d87e469a1ee9c0c6
poojakhare1 <147541085+poojakhare1@users.noreply.github.com>
2024-07-22 09:44:58 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String args[]){
+       // for(int counter = 0; counter<3; counter=counter+1){
+         //   System.out.println("hello pooja");
+       // }
+       //counter=i
+       for(int i=0; i<11; i++){
+        System.out.println(i);
+       }
+    }
+
+    
+}
